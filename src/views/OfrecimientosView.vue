5d169eeb92a579cdef32ec824a93a0c8d68bb38e
Ale Merchán <alejandramerchan25@gmail.com>
2024-04-25 18:53:08 -0500

    Cambiar nombre del equipo

--- src/views/OfrecimientosView.vue
+++ src/views/OfrecimientosView.vue
@@ -3,8 +3,7 @@
     <h2>OfreSer 🤝 SPL</h2>
     <h3>¿Qué tenemos para ofrecer?</h3>
     <!-- Nombre del Team -->
-    <h2>Hola soy título 2</h2>
-    <h2>Team A </h2>
+    <h2>The CROODS</h2>
     <v-row class="row-container mt-9">
       <v-col v-for="ofrecimiento in ofrecimientos" :key="ofrecimiento.id" cols="12" sm="6" md="4" lg="3">
         <v-card shaped class="mb-4">
